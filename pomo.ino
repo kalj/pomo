23d7605e9a558c4bd0684e12b1071c682bd574b6
Karl Ljungkvist <k.ljungkvist@gmail.com>
2020-10-10 19:09:24 +0200

    fix formatting

--- pomo.ino
+++ pomo.ino
@@ -5,8 +5,7 @@
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);//, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
 #define B1_PIN 6
 #define B2_PIN 5
@@ -14,23 +13,23 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);//, &Wire, OLED_RESET);
 #define B4_PIN 4
 
 enum class ButtonEvent {
-NoEvent,
-ShortPressB1,
-ShortPressB2,
-ShortPressB3,
-ShortPressB4,
-LongPressB1,
-LongPressB2,
-LongPressB3,
-LongPressB4
+    NoEvent,
+    ShortPressB1,
+    ShortPressB2,
+    ShortPressB3,
+    ShortPressB4,
+    LongPressB1,
+    LongPressB2,
+    LongPressB3,
+    LongPressB4
 };
 
 enum class State {
-Idle,
-Running,
-Paused,
-Ringing,
-Setup   
+    Idle,
+    Running,
+    Paused,
+    Ringing,
+    Setup
 };
 
 struct Time {
@@ -70,7 +69,7 @@ void process_buttons() {
     char b2 = digitalRead(B2_PIN);
     char b3 = digitalRead(B3_PIN);
     char b4 = digitalRead(B4_PIN);
-    
+
 }
 
 void process_timer() {
@@ -82,52 +81,52 @@ void process_timer() {
 
 void handle_events() {
     switch(state) {
-        case State::Running: {
-            if(timer_seconds_left <= 0) {
-                state = State::Ringing;
-            } else if(0) {
-                state = State::Paused;
-            } else if(0) {
-                state = State::Idle;
-            }
-            break;
+    case State::Running: {
+        if(timer_seconds_left <= 0) {
+            state = State::Ringing;
+        } else if(0) {
+            state = State::Paused;
+        } else if(0) {
+            state = State::Idle;
         }
-        case State::Paused: {
-            if(0) {
-                state = State::Running;
-                timer_end = now() + timer_seconds_left;
-            } else if(0) {
-                state = State::Idle;
-            }
-            break;
+        break;
+    }
+    case State::Paused: {
+        if(0) {
+            state = State::Running;
+            timer_end = now() + timer_seconds_left;
+        } else if(0) {
+            state = State::Idle;
         }
-        case State::Idle: {
-            if(0) {
-                uint16_t timer_length;
-                if(current_timer == TimerType::Work) {
-                    timer_length = work_setup_time.to_seconds();
-                } else {
-                    timer_length = rest_setup_time.to_seconds();
-                }
-                timer_seconds_left = timer_length;
-                state = State::Running;
-                timer_end = now() + timer_seconds_left;
+        break;
+    }
+    case State::Idle: {
+        if(0) {
+            uint16_t timer_length;
+            if(current_timer == TimerType::Work) {
+                timer_length = work_setup_time.to_seconds();
+            } else {
+                timer_length = rest_setup_time.to_seconds();
             }
-            break;
+            timer_seconds_left = timer_length;
+            state = State::Running;
+            timer_end = now() + timer_seconds_left;
         }
-        case State::Ringing: {
-            if(0) {
-                state = State::Idle;
-                current_timer = current_timer==TimerType::Work ? TimerType::Rest : TimerType::Work;
-            }
-            break;
+        break;
+    }
+    case State::Ringing: {
+        if(0) {
+            state = State::Idle;
+            current_timer = current_timer==TimerType::Work ? TimerType::Rest : TimerType::Work;
         }
-        case State::Setup: {
-            if(0) {
-                state = State::Idle;
-            }
-            break;
+        break;
+    }
+    case State::Setup: {
+        if(0) {
+            state = State::Idle;
         }
+        break;
+    }
     }
 }
 
@@ -148,7 +147,7 @@ void display_text(const char *txt, int8_t row, int8_t col, bool inverse=false, i
 }
 
 void display_time(Time t, int8_t row, int8_t col, bool m_inv, bool s_inv, int8_t size=1) {
-    
+
     display.setTextSize(size);
     display.setCursor(col,row);
     if(m_inv) {
@@ -160,7 +159,7 @@ void display_time(Time t, int8_t row, int8_t col, bool m_inv, bool s_inv, int8_t
         display.print("0");
     }
     display.print(t.min);
-    
+
     if(m_inv) {
         display.setTextColor(SSD1306_WHITE, SSD1306_BLACK);
     }
@@ -176,10 +175,10 @@ void display_time(Time t, int8_t row, int8_t col, bool m_inv, bool s_inv, int8_t
 }
 
 void update_display() {
-    
+
     display.clearDisplay();
 
-//    if(alarm_dim_display)
+    //    if(alarm_dim_display)
 
     display.fillCircle(25, 25, 12, SSD1306_WHITE);
 
@@ -207,7 +206,7 @@ void update_display() {
     }
     display_time(rest_time, 32+10, 66, true, false, 2);
 
-//    if(state != State::
+    //    if(state != State::
     // Draw indicator triangle
     int8_t indicator_col = 57;
     int8_t indicator_row = (current_timer == TimerType::Work ? 0 : 32)+12;
@@ -216,7 +215,6 @@ void update_display() {
     display.fillTriangle(indicator_col, indicator_row,
                          indicator_col+triangle_width-1, indicator_row+(triangle_height-1)/2,
                          indicator_col, indicator_row+triangle_height-1, SSD1306_WHITE);
-
 
     display.display();
 }
@@ -246,11 +244,11 @@ void loop() {
     handle_events();
 
     process_alarm();
-    
+
     update_display();
-    
-//     display.dim(true);
-//    delay(250);
-//    display.dim(false);
-//    delay(250);
+
+    //     display.dim(true);
+    //    delay(250);
+    //    display.dim(false);
+    //    delay(250);
 }
